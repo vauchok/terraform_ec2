b22ee3993c0389d229dac93522b48562a825273b
Ihar Vauchok <Ihar_Vauchok@epam.com>
2018-05-28 18:09:28 +0300

    fix tags

--- jenkins-slave.tf
+++ jenkins-slave.tf
@@ -12,8 +12,5 @@ module "jenkins-slave" {
   subnet_id = "${var.subnet_id}"
   instance_type = "${var.instance_type}"
   user_data = "${file(var.user_data)}"
-  tags {
-      created_by = "${lookup(var.tags,"created_by")}"
-      Name = "${var.instance_name}-${count.index}"
-  }
+  tags = "${var.tags}"
 }

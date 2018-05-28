155d9f1cc4a5bd74f5b8cb04fbcfb97349656fb3
Ihar Vauchok <Ihar_Vauchok@epam.com>
2018-05-28 14:27:14 +0300

    fix variables

--- jenkins-slave.tf
+++ jenkins-slave.tf
@@ -4,4 +4,15 @@ provider "aws" {
 
 module "jenkins-slave" {
   source = "./module"
+
+  ami = "${var.ami_id}"
+  count = "${var.number_of_instances}"
+  security_groups = "${var.security_groups}"
+  subnet_id = "${var.subnet_id}"
+  instance_type = "${var.instance_type}"
+  user_data = "${file(var.user_data)}"
+  tags {
+      created_by = "${lookup(var.tags,"created_by")}"
+      Name = "${var.instance_name}-${count.index}"
+  }
 }

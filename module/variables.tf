127dd6f28fd130e677ea463318485ce9e331545c
Ihar Vauchok <Ihar_Vauchok@epam.com>
2018-06-07 18:35:20 +0300

    add variables

--- module/variables.tf
+++ module/variables.tf
@@ -66,20 +66,174 @@ variable "private_ip" {
   description = "(Optional) Private IP address to associate with the instance in a VPC"
 }
 
-variable "instance_name" {
-  description = "Used to populate the Name tag. This is done in main.tf"
-}
-
-variable "number_of_instances" {
-  description = "Number of instances to make"
+variable "source_dest_check" {
+  description = "(Optional) Controls if traffic is routed to the instance when the destination address does not match the instance. Used for NAT or VPNs. Defaults true"
 }
 
 variable "user_data" {
-  description = "The path to a file with user_data for the instances"
+  description = "(Optional) The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument; see user_data_base64 instead"
+}
+
+variable "user_data_base64" {
+  description = "(Optional) Can be used instead of user_data to pass base64-encoded binary data directly. Use this instead of user_data whenever the value is not a valid UTF-8 string. For example, gzip-encoded user data must be base64-encoded and passed via this argument to avoid corruption"
+}
+
+variable "iam_instance_profile" {
+  description = "(Optional) The IAM Instance Profile to launch the instance with"
+}
+
+variable "ipv6_address_count" {
+  description = "(Optional) A number of IPv6 addresses to associate with the primary network interface. Amazon EC2 chooses the IPv6 addresses from the range of your subnet"
 }
 
 variable "tags" {
-  default = {
-    created_by = "terraform"
- }
+  description = "(Optional) A mapping of tags to assign to the resource"
+}
+
+variable "volume_tags" {
+  description = "(Optional) A mapping of tags to assign to the devices created by the instance at launch time"
+}
+
+variable "root_block_device" {
+  description = "(Optional) Customize details about the root block device of the instance. See Block Devices below for details"
+  default = {}
+}
+
+variable "ebs_block_device" {
+  description = "(Optional) Additional EBS block devices to attach to the instance. See Block Devices below for details"
+  default = {}
+}
+
+variable "ephemeral_block_device" {
+  description = "(Optional) Customize Ephemeral (also known as Instance Store) volumes on the instance. See Block Devices below for details"
+  default = {}
+}
+
+variable "network_interface" {
+  description = "(Optional) Customize network interfaces to be attached at instance boot time. See Network Interfaces below for more details"
+  default = {}
+}
+
+variable "credit_specification" {
+  description = "(Optional) Customize the credit specification of the instance. See Credit Specification below for more details"
+  default = {}
+}
+
+
+# timeouts
+# https://www.terraform.io/docs/configuration/resources.html#timeouts 
+variable "create" {
+  description = "(Defaults to 10 mins) Used when launching the instance (until it reaches the initial running state)"
+}
+
+variable "update" {
+  description = "(Defaults to 10 mins) Used when stopping and starting the instance when necessary during update - e.g. when changing instance type"
+}
+
+variable "delete" {
+  description = "(Defaults to 20 mins) Used when terminating the instance"
+}
+
+
+# Block Devices
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/block-device-mapping-concepts.html
+# Currently, changes to *_block_device configuration of existing resources cannot be automatically detected by Terraform.
+# After making updates to block device configuration, resource recreation can be manually triggered by using the taint command.
+# https://www.terraform.io/docs/commands/taint.html
+
+# root_block_device. Modifying any of the root_block_device settings requires resource replacement:
+variable "volume_type_root" {
+  description = "(Optional) The type of volume. Can be 'standard', 'gp2', or 'io1'"
+}
+
+variable "volume_size_root" {
+  description = "(Optional) The size of the volume in gigabytes"
+}
+
+variable "iops_root" {
+  description = "(Optional) The amount of provisioned IOPS. This is only valid for volume_type of 'io1', and must be specified if using that type"
+}
+
+variable "delete_on_termination_root" {
+  description = "(Optional) Whether the volume should be destroyed on instance termination"
+  default = true
+}
+
+# ebs_block_device. Modifying any ebs_block_device currently requires resource replacement.
+# ebs_block_device cannot be mixed with external aws_ebs_volume + aws_volume_attachment resources:
+variable "device_name_ebs" {
+  description = "The name of the device to mount (available for ephemeral_block_device too)"
+}
+
+variable "snapshot_id_ebs" {
+  description = "(Optional) The Snapshot ID to mount"
+}
+variable "volume_type_ebs" {
+  description = "(Optional) The type of volume. Can be 'standard', 'gp2', or 'io1'"
+}
+
+variable "volume_size_ebs" {
+  description = "(Optional) The size of the volume in gigabytes"
+}
+
+variable "iops_ebs" {
+  description = "(Optional) The amount of provisioned IOPS. This is only valid for volume_type of 'io1', and must be specified if using that type"
+}
+
+variable "delete_on_termination_ebs" {
+  description = "(Optional) Whether the volume should be destroyed on instance termination"
+  default = true
+}
+
+variable "encrypted_ebs" {
+  description = "(Optional) Enables EBS encryption on the volume. Cannot be used with snapshot_id"
+  default = false
+}
+
+# ephemeral_block_device
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/InstanceStorage.html#InstanceStoreDeviceNames
+variable "device_name_ephemeral" {
+  description = "The name of the device to mount"
+}
+
+variable "virtual_name_ephemeral" {
+  description = "(Optional) The Instance Store Device Name (e.g. 'ephemeral0')"
+}
+
+variable "no_device_ephemeral" {
+  description = "(Optional) Suppresses the specified device included in the AMI's block device mapping"
+}
+
+
+# Network interfaces
+variable "device_index" {
+  description = "(Required) The integer index of the network interface attachment. Limited by instance type"
+}
+variable "network_interface_id" {
+  description = "(Required) The ID of the network interface to attach"
+}
+
+variable "delete_on_termination_network_interface" {
+  description = "(Optional) Whether or not to delete the network interface on instance termination. Currently, the only valid value is false, as this is only supported when creating new network interfaces when launching an instance"
+  default = false
+}
+
+
+# Credit specification
+# can be applied/modified to the EC2 Instance at any time
+variable "cpu_credits" {
+  description = "(Optional) The credit option for CPU usage"
+}
+
+
+variable "delete" {
+  description = "(Defaults to 20 mins) Used when terminating the instance"
+}
+
+variable "delete" {
+  description = "(Defaults to 20 mins) Used when terminating the instance"
+}
+
+variable "delete" {
+  description = "(Defaults to 20 mins) Used when terminating the instance"
 }

45b26730e72c492b5204de7fb4d99ee4e19e2e60
Ihar Vauchok <Ihar_Vauchok@epam.com>
2018-06-18 13:33:49 +0300

    add

--- jenkins-slave.tf
+++ jenkins-slave.tf
@@ -14,5 +14,4 @@ module "jenkins-slave" {
   vpc_security_group_ids = "${var.vpc_security_group_ids}"
   instance_type = "${var.instance_type}"
   user_data = "${var.user_data}"
-  tags = "${var.tags}"
 }

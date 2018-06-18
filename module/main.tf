9986728dec3e9c5bc410c069b8a20a5162161ab7
Ihar Vauchok <ihar.vauchok@wolterskluwer.com>
2018-06-18 13:48:39 +0100

    add

--- module/main.tf
+++ module/main.tf
@@ -13,30 +13,27 @@ resource "aws_instance" "ec2_instance" {
 
   #security_groups = "${var.security_groups}"
 
-  associate_public_ip_address = "${var.associate_public_ip_address}"
-  private_ip                  = "${var.private_ip}"
-  ipv6_address_count          = "${var.ipv6_address_count}"
-  ipv6_addresses              = "${var.ipv6_addresses}"
-  ebs_optimized          = "${var.ebs_optimized}"
-  volume_tags            = "${var.volume_tags}"
-  root_block_device      = "${var.root_block_device}"
-  ebs_block_device       = "${var.ebs_block_device}"
-  ephemeral_block_device = "${var.ephemeral_block_device}"
+  associate_public_ip_address          = "${var.associate_public_ip_address}"
+  private_ip                           = "${var.private_ip}"
+  ipv6_address_count                   = "${var.ipv6_address_count}"
+  ipv6_addresses                       = "${var.ipv6_addresses}"
+  ebs_optimized                        = "${var.ebs_optimized}"
+  volume_tags                          = "${var.volume_tags}"
+  root_block_device                    = "${var.root_block_device}"
+  ebs_block_device                     = "${var.ebs_block_device}"
+  ephemeral_block_device               = "${var.ephemeral_block_device}"
   source_dest_check                    = "${var.source_dest_check}"
   disable_api_termination              = "${var.disable_api_termination}"
   instance_initiated_shutdown_behavior = "${var.instance_initiated_shutdown_behavior}"
   placement_group                      = "${var.placement_group}"
   tenancy                              = "${var.tenancy}"
-
   tags = "${merge(var.tags, map("Name", var.number_of_instances > 1 ? format("%s-%d", var.instance_name, count.index+1) : var.instance_name))}"
-  
   lifecycle {
     # Due to several known issues in Terraform AWS provider related to arguments of aws_instance:
     # (eg, https://github.com/terraform-providers/terraform-provider-aws/issues/2036)
     # we have to ignore changes in the following arguments
     ignore_changes = ["private_ip", "root_block_device"]
   }
-
   credit_specification {
     cpu_credits = "${var.cpu_credits}"
   }

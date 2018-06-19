0508b0acc54c767166ba8f6db0f0d711c13952bb
Ihar Vauchok <Ihar_Vauchok@epam.com>
2018-06-19 16:53:36 +0300

    add

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 yum -y install epel-release
-yum -y install git, python-pip
-pip install --upgrade pip
+yum -y install git, python-pip unzip
 pip install awscli
+
+mkdir -p /opt/jenkins/jenkins-slave
+chown -R centos:centos /opt/jenkins/jenkins-slave
+
+# terraform installation
+wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+unzip terraform_0.11.7_linux_amd64.zip -d /usr/bin/ && rm -rf terraform_0.11.7_linux_amd64.zip
